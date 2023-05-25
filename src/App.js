f70120ca54d974d1ecf12a991663dd8ce5fc4ea9
1sk4ndar <134580370+1sk4ndar@users.noreply.github.com>
2023-05-25 10:48:19 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,202 @@
+
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+    
+     <ul className='menu'>
+      <li className='sub'>Tortas</li><input className='numero' type='number'></input>
+      <ul>
+      <input type='checkbox'></input><li className='menu_items'>Bolillo</li>
+      <input type='checkbox'></input><li  className='menu_items'>Telera</li>
+      <input type='checkbox'></input><li className='menu_items'>Chile molido</li>
+      <input type='checkbox'></input><li className='menu_items'>Chile jalapeño</li>
+      <input type='checkbox'></input><li className='menu_items'>Con todo</li>
+      <input type='checkbox'></input><li className='menu_items'>Sin chile</li>
+      <input type='checkbox'></input><li className='menu_items'>Sin cebolla</li>
+      </ul>
+      <ul>
+      <input type='checkbox'></input><li className='menu_items'>Sin jitomate</li>
+      <input type='checkbox'></input><li className='menu_items'>Sin lechuga</li>
+      <input type='checkbox'></input><li className='menu_items'>Especial de pierna</li>
+      <input type='checkbox'></input><li className='menu_items'>Especial de pollo</li>
+      <input type='checkbox'></input><li className='menu_items'>Pierna</li>
+      <input type='checkbox'></input><li className='menu_items'>Pollo</li>
+      <input type='checkbox'></input><li className='menu_items'>Jamon con queso</li>
+      </ul>
+      <li className='sub'>Sandwich</li><input className='numero' type='number'></input>
+      <ul>
+      <input type='checkbox'></input><li className='menu_items'>Pan blanco</li>
+      <input type='checkbox'></input><li  className='menu_items'>Pan integral</li>
+      <input type='checkbox'></input><li className='menu_items'>Chile molido</li>
+      <input type='checkbox'></input><li className='menu_items'>Chile jalapeño</li>
+      <input type='checkbox'></input><li className='menu_items'>Con todo</li>
+      <input type='checkbox'></input><li className='menu_items'>Sin chile</li>
+      <input type='checkbox'></input><li className='menu_items'>Sin cebolla</li>
+      </ul>
+      <ul>
+      <input type='checkbox'></input><li className='menu_items'>Sin jitomate</li>
+      <input type='checkbox'></input><li className='menu_items'>Sin lechuga</li>
+      <input type='checkbox'></input><li className='menu_items'>Especial de pierna</li>
+      <input type='checkbox'></input><li className='menu_items'>Especial de pollo</li>
+      <input type='checkbox'></input><li className='menu_items'>Pierna</li>
+      <input type='checkbox'></input><li className='menu_items'>Pollo</li>
+      <input type='checkbox'></input><li className='menu_items'>Jamon con queso</li>
+      </ul>
+      <li className='sub'>Burritos</li><input className='numero' type='number'></input>
+      <ul>
+      <input type='checkbox'></input><li className='menu_items'>Pierna</li>
+      <input type='checkbox'></input><li  className='menu_items'>Pollo</li>
+      <input type='checkbox'></input><li className='menu_items'>Verdura encima</li>
+      <input type='checkbox'></input><li className='menu_items'>Verdura al lado</li>
+      <input type='checkbox'></input><li className='menu_items'>Sin cebolla</li>
+      <input type='checkbox'></input><li className='menu_items'>Sin salsa picada</li>
+      <input type='checkbox'></input><li className='menu_items'>Sin lechuga</li>
+      <input type='checkbox'></input><li className='menu_items'>Con todo</li>
+      </ul>
+      <li className='sub'>Sincronizada</li><input className='numero' type='number'></input>
+      <ul>
+      <input type='checkbox'></input><li className='menu_items'>Pierna</li>
+      <input type='checkbox'></input><li  className='menu_items'>Pollo</li>
+      <input type='checkbox'></input><li className='menu_items'>Sencilla</li>
+      <input type='checkbox'></input><li className='menu_items'>Sin cebolla</li>
+      <input type='checkbox'></input><li className='menu_items'>Sin jitomate</li>
+      <input type='checkbox'></input><li className='menu_items'>Sin lechuga</li>
+      <input type='checkbox'></input><li className='menu_items'>Con todo</li>
+      <input type='checkbox'></input><li className='menu_items'>Chile molido</li>
+      <input type='checkbox'></input><li className='menu_items'>Chile jalapeño</li>
+      <input type='checkbox'></input><li className='menu_items'>Sin chile</li>
+      </ul>
+      <li className='sub'>Quesadillas</li><input className='numero' type='number'></input>
+      <li className='sub'>Tostadas</li><input className='numero' type='number'></input>
+      <ul>
+      <input type='checkbox'></input><li className='menu_items'>Pierna</li>
+      <input type='checkbox'></input><li  className='menu_items'>Pollo</li>
+      <input type='checkbox'></input><li  className='menu_items'>Panela</li>
+      <input type='checkbox'></input><li  className='menu_items'>Combinada</li>
+      <input type='checkbox'></input><li  className='menu_items'>Frijoles</li>
+      <input type='checkbox'></input><li  className='menu_items'>Crema</li>
+      <input type='checkbox'></input><li  className='menu_items'>Crema encima</li>
+      <input type='checkbox'></input><li className='menu_items'>Sin cebolla</li>
+      <input type='checkbox'></input><li className='menu_items'>Sin salsa picada</li>
+      <input type='checkbox'></input><li className='menu_items'>Sin lechuga</li>
+      <input type='checkbox'></input><li className='menu_items'>Con todo</li>
+      </ul>
+      <li className='sub'>Bionicos</li><input className='numero' type='number'></input>
+      <ul><input type='checkbox'></input><li className='menu_items'>Grande</li>
+      <input type='checkbox'></input><li className='menu_items'>Chico</li>
+      <input type='checkbox'></input><li className='menu_items'>Toda la Fruta</li>
+      <input type='checkbox'></input><li className='menu_items'>Sin melon</li>
+      <input type='checkbox'></input><li className='menu_items'>Sin papaya</li>
+      <input type='checkbox'></input><li className='menu_items'>Sin manzana</li>
+      <input type='checkbox'></input><li className='menu_items'>Sin platano</li>
+      <input type='checkbox'></input><li className='menu_items'>Sin fresa</li>
+      </ul>
+      <ul>
+      <input type='checkbox'></input><li className='menu_items'>Sin guayaba</li>
+      <input type='checkbox'></input><li className='menu_items'>Con todo el cereal</li>
+      <input type='checkbox'></input><li className='menu_items'>Granola</li>
+      <input type='checkbox'></input><li className='menu_items'>Nuez</li>
+      <input type='checkbox'></input><li className='menu_items'>Pasas</li>
+      <input type='checkbox'></input><li className='menu_items'>Almendras</li>
+      <input type='checkbox'></input><li className='menu_items'>Coco</li>
+      </ul>
+      <li className='sub'>Yogurt</li><input className='numero' type='number'></input>
+      <ul>
+      <input type='checkbox'></input><li className='menu_items'>Grande</li>
+      <input type='checkbox'></input><li className='menu_items'>Chico</li>
+      <input type='checkbox'></input><li className='menu_items'>Toda la Fruta</li>
+      <input type='checkbox'></input><li className='menu_items'>Sin melon</li>
+      <input type='checkbox'></input><li className='menu_items'>Sin papaya</li>
+      <input type='checkbox'></input><li className='menu_items'>Sin manzana</li>
+      <input type='checkbox'></input><li className='menu_items'>Sin platano</li>
+      <input type='checkbox'></input><li className='menu_items'>Sin fresa</li>
+      </ul>
+      <ul>
+      <input type='checkbox'></input><li className='menu_items'>Sin guayaba</li>
+      <input type='checkbox'></input><li className='menu_items'>Con todo el cereal</li>
+      <input type='checkbox'></input><li className='menu_items'>Granola</li>
+      <input type='checkbox'></input><li className='menu_items'>Nuez</li>
+      <input type='checkbox'></input><li className='menu_items'>Pasas</li>
+      <input type='checkbox'></input><li className='menu_items'>Almendras</li>
+      <input type='checkbox'></input><li className='menu_items'>Coco</li>
+      </ul>
+      
+      <li className='sub'>Jugo de naranja</li><input className='numero' type='number'></input>
+      <ul>
+      <input type='checkbox'></input><li className='menu_items'>Chico</li>
+      <input type='checkbox'></input><li className='menu_items'>Mediano</li>
+      <input type='checkbox'></input><li className='menu_items'>Grande</li>
+      </ul>
+      
+      <li className='sub'>Jugo de zanahoria</li><input className='numero' type='number'></input>
+      <ul>
+      <input type='checkbox'></input><li className='menu_items'>Chico</li>
+      <input type='checkbox'></input><li className='menu_items'>Mediano</li>
+      <input type='checkbox'></input><li className='menu_items'>Grande</li>
+      </ul>
+      
+      <li className='sub'>Jugo de betavel</li><input className='numero' type='number'></input>
+      <ul>
+      <input type='checkbox'></input><li className='menu_items'>Chico</li>
+      <input type='checkbox'></input><li className='menu_items'>Mediano</li>
+      <input type='checkbox'></input><li className='menu_items'>Grande</li>
+      <input type='checkbox'></input><li className='menu_items'>Naranja</li>
+      <input type='checkbox'></input><li className='menu_items'>Zanahoria</li>
+      </ul>
+      
+      <li className='sub'>Jugo verde</li><input className='numero' type='number'></input>
+      <ul>
+      <input type='checkbox'></input><li className='menu_items'>Chico</li>
+      <input type='checkbox'></input><li className='menu_items'>Mediano</li>
+      <input type='checkbox'></input><li className='menu_items'>Grande</li>
+      <input type='checkbox'></input><li className='menu_items'>Naranja</li>
+      <input type='checkbox'></input><li className='menu_items'>Toronja</li>
+      </ul>
+      
+      <li className='sub'>Jugo de piña</li><input className='numero' type='number'></input>
+      <ul>
+      <input type='checkbox'></input><li className='menu_items'>Chico</li>
+      <input type='checkbox'></input><li className='menu_items'>Mediano</li>
+      <input type='checkbox'></input><li className='menu_items'>Grande</li>
+      </ul>
+      
+      <li className='sub'>Jugo combinado</li><input className='numero' type='number'></input>
+      <ul>
+      <input type='checkbox'></input><li className='menu_items'>Chico</li>
+      <input type='checkbox'></input><li className='menu_items'>Mediano</li>
+      <input type='checkbox'></input><li className='menu_items'>Grande</li>
+      </ul>
+      
+      <li className='sub'>Jugo de toronja</li><input className='numero' type='number'></input>
+      <ul>
+      <input type='checkbox'></input><li className='menu_items'>Chico</li>
+      <input type='checkbox'></input><li className='menu_items'>Mediano</li>
+      <input type='checkbox'></input><li className='menu_items'>Grande</li>
+      </ul>
+      
+      <li className='sub'>Chocomil</li><input className='numero' type='number'></input>
+      <ul>
+      <input type='checkbox'></input><li className='menu_items'>Chico</li>
+      <input type='checkbox'></input><li className='menu_items'>Mediano</li>
+      <input type='checkbox'></input><li className='menu_items'>Grande</li>
+      <input type='checkbox'></input><li className='menu_items'>Con canela</li> 
+      </ul>
+      
+      
+      <li className='sub'>Licuado</li><input className='numero' type='number'></input>
+      <ul>
+      <input type='checkbox'></input><li className='menu_items'>Chico</li>
+      <input type='checkbox'></input><li className='menu_items'>Mediano</li>
+      <input type='checkbox'></input><li className='menu_items'>Grande</li>
+      <input type='checkbox'></input><li className='menu_items'>Con canela</li>
+      <input type='checkbox'></input><li className='menu_items'>Fresa</li> 
+      <input type='checkbox'></input><li className='menu_items'>Platano</li>
+      </ul>
+        
+      </ul> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/tortas.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const tortas = () => {
+  return (
+    <div>
+      <h1>Tortas</h1>
+      <ul>
+        <li>Telera</li>
+        <li>Bolillo</li>
+        <li>Con todo</li>
+        <li>Jalapeño</li>
+        <li>Molido</li>
+      </ul>
+    </div>
+  )
+}
